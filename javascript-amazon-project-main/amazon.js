4e1bcbb90351dd9543798a8bcd63c50ce9e87061
creator-chandru <creatorchandru31@gmail.com>
2026-04-14 13:50:09 +0530

    made functions for formatting currency

--- javascript-amazon-project-main/amazon.js
+++ javascript-amazon-project-main/amazon.js
@@ -21,7 +21,7 @@ products.forEach((product)=>{
           </div>
 
           <div class="product-price">
-            ${(product.priceCents / 100).toFixed(2)}
+            ${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
