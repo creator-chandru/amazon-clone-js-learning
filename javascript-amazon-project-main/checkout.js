41d6405e6b96e8863059ea57472576135cff3ffe
creator-chandru <creatorchandru31@gmail.com>
2026-04-15 09:04:19 +0530

    generated HTML for the delivery options

--- javascript-amazon-project-main/checkout.js
+++ javascript-amazon-project-main/checkout.js
@@ -1,8 +1,13 @@
 import {cart, removeFromCart} from './data/cart.js';
 import {products} from './data/products.js';
 import { formatCurrency } from './utilities/money.js';
+import dayjs from 'https://cdn.skypack.dev/dayjs';
+import {deliveryOptions} from './data/deliveryOptions.js';
+
 let cartSummaryHTML = '';
 
+console.log(dayjs());
+console.log(dayjs().format('dddd, MMMM, D'));
 cart.forEach((cartItem) => {
     const productId = cartItem.productId;
     let matchingProduct;
@@ -45,49 +50,36 @@ cart.forEach((cartItem) => {
             <div class="delivery-options-title">
                 Choose a delivery option:
             </div>
-            <div class="delivery-option">
-                <input type="radio" checked
-                class="delivery-option-input"
-                name="delivery-option-${matchingProduct.id}">
-                <div>
-                <div class="delivery-option-date">
-                    Tuesday, June 21
-                </div>
-                <div class="delivery-option-price">
-                    FREE Shipping
-                </div>
-                </div>
-            </div>
-            <div class="delivery-option">
-                <input type="radio"
-                class="delivery-option-input"
-                name="delivery-option-${matchingProduct.id}">
-                <div>
-                <div class="delivery-option-date">
-                    Wednesday, June 15
-                </div>
-                <div class="delivery-option-price">
-                    $4.99 - Shipping
-                </div>
-                </div>
-            </div>
-            <div class="delivery-option">
-                <input type="radio"
-                class="delivery-option-input"
-                name="delivery-option-${matchingProduct.id}">
-                <div>
-                <div class="delivery-option-date">
-                    Monday, June 13
-                </div>
-                <div class="delivery-option-price">
-                    $9.99 - Shipping
-                </div>
-                </div>
-            </div>
+            ${deliveryOptionHTML (matchingProduct)}
             </div>
         </div>
         </div>`
 });
+
+function deliveryOptionHTML (matchingProduct){
+    let HTML = '';
+
+    deliveryOptions.forEach((deliveryOption)=>{
+        let deliveryDate = dayjs().add(deliveryOption.deliveryDays,'days');
+        const dateString = deliveryDate.format('dddd, MMMM D');
+        let priceString = deliveryOption.deliveryPrice ? `$${formatCurrency(deliveryOption.deliveryPrice)} - ` : 'FREE';
+
+        HTML += `<div class="delivery-option">
+                    <input type="radio"
+                    class="delivery-option-input"
+                    name="delivery-option-${matchingProduct.id}">
+                    <div>
+                    <div class="delivery-option-date">
+                        ${dateString}
+                    </div>
+                    <div class="delivery-option-price">
+                        ${priceString} Shipping
+                    </div>
+                    </div>
+                </div>`;
+    });
+    return HTML;
+}
 
 document.querySelector('.order-summary').innerHTML = cartSummaryHTML;
 document.querySelectorAll('.delete-quantity-link').forEach((del)=>{
---
+++ javascript-amazon-project-main/data/deliveryOptions.js
@@ -0,0 +1,13 @@
+export const deliveryOptions =  [{
+    id : '1',
+    deliveryDays : '7 days',
+    deliveryPrice : 0
+},{
+    id : '2',
+    deliveryDays : '5 days',
+    deliveryPrice : 499
+},{
+    id : '3',
+    deliveryDays : '3 days',
+    deliveryPrice : 999
+}];
