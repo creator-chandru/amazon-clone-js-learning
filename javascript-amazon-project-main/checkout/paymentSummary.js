18c2d2e5b7f00bb4e9004d05dd3b3e3eb34b63cc
creator-chandru <creatorchandru31@gmail.com>
2026-04-16 17:34:03 +0530

    generated paymentSummary HTML using the payment data

--- javascript-amazon-project-main/checkout/paymentSummary.js
+++ javascript-amazon-project-main/checkout/paymentSummary.js
@@ -1,6 +1,7 @@
 import {findProduct} from '../data/products.js';
 import {cart} from '../data/cart.js';
 import {findDeliveryMethod} from '../data/deliveryOptions.js';
+import {formatCurrency} from '../utilities/money.js';
 
 export function renderPaymentsummary () {
     let totalPrice = 0;
@@ -14,4 +15,43 @@ export function renderPaymentsummary () {
     });
     console.log(totalPrice);
     console.log(shippingPrice);
+    const totalBeforeTax = totalPrice + shippingPrice;
+    console.log(totalBeforeTax);
+    const tax = 0.10 * totalBeforeTax;
+    console.log(tax);
+    let paymentSummaryHTML = `
+          <div class="payment-summary-title">
+            Order Summary
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Items (3):</div>
+            <div class="payment-summary-money">$${formatCurrency(totalPrice)}</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Shipping &amp; handling:</div>
+            <div class="payment-summary-money">$${formatCurrency(shippingPrice)}</div>
+          </div>
+
+          <div class="payment-summary-row subtotal-row">
+            <div>Total before tax:</div>
+            <div class="payment-summary-money">$${formatCurrency(totalBeforeTax)}</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Estimated tax (10%):</div>
+            <div class="payment-summary-money">$${formatCurrency(tax)}</div>
+          </div>
+
+          <div class="payment-summary-row total-row">
+            <div>Order total:</div>
+            <div class="payment-summary-money">$${formatCurrency(totalBeforeTax + tax)}</div>
+          </div>
+
+          <button class="place-order-button button-primary">
+            Place your order
+          </button>`;
+
+          document.querySelector('.payment-summary').innerHTML = paymentSummaryHTML;
 }
