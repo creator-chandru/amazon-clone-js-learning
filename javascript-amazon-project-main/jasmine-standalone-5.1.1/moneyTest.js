79ee5efa813926467800a5a28c466094dab069f1
creator-chandru <creatorchandru31@gmail.com>
2026-04-16 18:50:40 +0530

    add money tests

---
+++ javascript-amazon-project-main/jasmine-standalone-5.1.1/moneyTest.js
@@ -0,0 +1,13 @@
+import {formatCurrency} from '../utilities/money.js';
+
+describe('test suite: formatCurrency',()=>{
+    it('converts cents into dollars', ()=>{
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+    it('converts zero correctly',()=>{
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
+    it('rounds up to the nearest cent',()=>{
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
+});
