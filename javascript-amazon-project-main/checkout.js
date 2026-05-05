b6e3d6471983e727719ecd3705b316cd80472406
creator-chandru <creatorchandru31@gmail.com>
2026-05-05 17:25:34 +0530

    learn promises and convert code to promises

--- javascript-amazon-project-main/checkout.js
+++ javascript-amazon-project-main/checkout.js
@@ -1,9 +1,40 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import {loadProducts} from "./data/products.js";
+import {loadCart} from "./data/cart.js";
 //import './data/backend-practice.js';
+
+Promise.all([
+    new Promise((resolve)=>{
+        loadProducts(()=>{
+            resolve('value1');
+        });
+    }),
+    new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        });
+    })
+]).then((values)=>{
+    console.log(values);
+    renderOrderSummary();
+    renderPaymentSummary();
+})
+
+
+
+
+
+/*new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+    });
+}).then(()=>{
+    renderPaymentSummary();
+    renderOrderSummary();
+})
 
 loadProducts(()=>{
     renderPaymentSummary();
     renderOrderSummary();
-});
+});*/
